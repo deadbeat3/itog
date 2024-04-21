7f758ea229bcf13ced6deaf00c477263dd53c6a3
deadbeat3 <167681603+deadbeat3@users.noreply.github.com>
2024-04-21 23:07:08 +0300

    Add files via upload

---
+++ commscr.js
@@ -0,0 +1,37 @@
+let comments = []
+comments = JSON.parse(localStorage.getItem('comments')) || [{Name : 'Фаггио', Comment : 'Я заднеприводный'}]
+const cont = document.querySelector('body div.cont')
+
+function load(){
+    for (let comment of comments){
+        const com = document.createElement('div'),
+            name = document.createElement('p'),
+            text = document.createElement('p')
+    
+    
+        com.classList.add('comment')
+        name.classList.add('name')
+    
+        name.textContent = comment.Name
+        text.textContent = comment.Comment
+    
+        cont.append(com)
+        com.append(name)
+        com.append(text)
+    }
+}
+load()
+
+document.querySelector('div.leave-comment button.sort').addEventListener('click', () => {
+    let name = document.querySelector('div.leave-comment input#name'),
+        comment = document.querySelector('div.leave-comment textarea#com')
+
+    if (name.value === '' || comment.value === '') return alert('хуй')
+
+    comments.push({Name : name.value, Comment : comment.value})
+    localStorage.setItem('comments', JSON.stringify(comments))
+
+    name.value = ''; comment.value = ''
+    cont.innerHTML = ''
+    load()
+})
